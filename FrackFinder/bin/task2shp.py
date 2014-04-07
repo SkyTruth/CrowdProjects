ae585fb9caf75af2edcf86c3b612980c45f151f2
Kevin Wurster <kevin@skytruth.org>
2014-04-07 15:27:52 -0400

    Additional help flags.  Use task2shp.py --help-info for a list.

--- FrackFinder/bin/task2shp.py
+++ FrackFinder/bin/task2shp.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
 
+# See global __license__ variable for license information
+
+
+"""
+Convert a FrackFinder JSON export to a shapefile
+containing 1 point per pond and aggregated response
+metrics.
+"""
+
+
 import os
 import sys
 import json
+import inspect
 from os.path import isfile
 from os.path import basename
 import ogr
@@ -16,11 +27,38 @@ DEBUG = False
 
 # Build information
 __author__ = 'Kevin Wurster'
-__organization__ = 'SkyTruth'
-__doc__ = """
-Convert a FrackFinder JSON export to a shapefile
-containing 1 point per pond and aggregated response
-metrics.
+__copyright__ = 'Copyright (c) 2014, SkyTruth'
+__version__ = '0.1'
+__release__ = '2014/04/07'
+__docname__ = basename(inspect.getfile(inspect.currentframe()))
+__license__ = """
+Copyright (c) 2014, SkyTruth
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the {organization} nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 
@@ -29,9 +67,10 @@ def print_usage():
     Command line usage information
     """
     print("")
-    print("Usage: [options] task.json task_run.json outfile.shp")
+    print("Usage: %s [options] task.json task_run.json outfile.shp" % __docname__)
     print("")
     print("Options:")
+    print("  --help-info -> Print out a list of help related flags")
     print("  --of=driver -> Output driver name/file type - default='ESRI Shapefile'")
     print("  --epsg=int  -> EPSG code for coordinates in task.json - default=4326")
     print("")
@@ -45,6 +84,58 @@ def pdebug(message):
     global DEBUG
     if DEBUG is True:
         print(message)
+
+
+def print_license():
+    """
+    Print out license information
+    """
+    print('\n' + __license__ + '\n')
+    return 1
+
+
+def print_help():
+    """
+    Detailed help information
+    """
+    print("")
+    print("%s Detailed Help" % __docname__)
+    print("--------------" + "-" * len(__docname__))
+    print("PyBossa exports tasks in two ways: task.json and task_run.json")
+    print("This utility looks at each task in task.json and sifts through")
+    print("its matching task_run.json to calculate a variety of metrics,")
+    print("like how many times the task was shown, how many times the crowd,")
+    print("how many times the crowd picked each possible answer, and how")
+    print("confident the crowd was in its decision.")
+    print("")
+    print("It is possible for the crowd to be split and pick two different")
+    print("choices an equal number of times.  The output for these cases")
+    print("is pipe delimited.")
+    print("")
+    return 1
+
+
+def print_help_info():
+    """
+    Print a list of help related flags
+    """
+    print("")
+    print("Help flags:")
+    print("  --help    -> More detailed description of this utility")
+    print("  --usage   -> Arguments, parameters, flags, options, etc.")
+    print("  --version -> Version and ownership information")
+    print("  ")
+
+
+def print_version():
+    """
+    Print script version information
+    """
+    print("")
+    print('%s version %s - released %s' % (__docname__, __version__, __release__))
+    print(__copyright__)
+    print("")
+    return 1
 
 
 def get_crowd_selection(selection_count, selection_map):
@@ -173,8 +264,20 @@ def main(args):
     arg_error = False
     for arg in args:
 
+        # Help arguments
+        if arg in ('--license', '-license'):
+            return print_license()
+        elif arg in ('--usage', '-usage'):
+            return print_usage()
+        elif arg in ('--version', '-version'):
+            return print_version()
+        elif arg in ('--help', '-help'):
+            return print_help()
+        elif arg in ('--help-info', '-help-info', '--helpinfo', '--helpinfo'):
+            return print_help_info()
+
         # Outfile specific options
-        if '--of=' in arg:
+        elif '--of=' in arg:
             outfile_driver = arg.split('=', 1)[1]
         elif '--epsg=' in arg:
             outfile_epsg_code = int(arg.split('=', 1)[1])
