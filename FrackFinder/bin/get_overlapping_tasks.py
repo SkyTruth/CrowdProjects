1eebdd68da7ef8706ddd8b2ae852512475315f72
Kevin Wurster <kevin@skytruth.org>
2014-04-10 18:07:47 -0400

    Working

--- FrackFinder/bin/get_overlapping_tasks.py
+++ FrackFinder/bin/get_overlapping_tasks.py
@@ -20,14 +20,16 @@ def get_location(task):
     return lat + lng + '---' + year
 
 
-def get_num_task_runs(task, task_runs):
+def is_task_complete(task, task_runs, redundancy):
     count = 0
-    task_id = str(task['id'])
+    task_id = int(task['id'])
     for tr in task_runs:
-        tr_id = tr['task_id']
+        tr_id = int(tr['task_id'])
         if task_id == tr_id:
             count += 1
-    return count
+            if count >= redundancy:
+                return True
+    return False
 
 
 def main(args):
@@ -37,6 +39,7 @@ def main(args):
     public_task_runs = '../DartFrog/QAQC/complete-tasks/export/public/task_run.json'
     internal_tasks = '../DartFrog/QAQC/complete-tasks/export/first-internal/task.json'
     internal_task_runs = '../DartFrog/QAQC/complete-tasks/export/first-internal/task_run.json'
+    outfile = 'OVERLAPPING_TASKS.json'
 
     # Make sure the files exist
     for filepath in (public_tasks, public_task_runs, internal_tasks, internal_task_runs):
@@ -45,6 +48,7 @@ def main(args):
             return 1
 
     # Open all files and cache as JSON
+    print("Parsing input files...")
     json_content = {'public_tasks': public_tasks,
                     'public_task_runs': public_task_runs,
                     'internal_tasks': internal_tasks,
@@ -55,21 +59,38 @@ def main(args):
 
     # Update user
     for name, content in json_content.iteritems():
-        print(name + ' = ' + str(len(content))
+        print('  ' + name + ' = ' + str(len(content)))
 
     # Get all completed tasks
-    completed_tasks = {'public_tasks': [],
-                       'internal_tasks': []}
+    print("Getting completed tasks...")
+    completed_public_tasks = []
+    completed_internal_tasks = []
     for task in json_content['public_tasks']:
-        if get_num_task_runs(task, json_content['public_task_runs']) >= 10:
-            completed_tasks['public_tasks'].append(task)
+        if is_task_complete(task, json_content['public_task_runs'], 10):
+            completed_public_tasks.append(task)
+    print("  public_tasks = %s" % str(len(completed_public_tasks)))
     for task in json_content['internal_tasks']:
-        if get_num_task_runs(task, json_content['internal_task_runs']) >= 3:
-            completed_tasks['internal_tasks'].append(task)
+        if is_task_complete(task, json_content['internal_task_runs'], 3):
+            completed_internal_tasks.append(task)
+    print("  internal_tasks = %s" % str(len(completed_internal_tasks)))
 
-    # Update user
-    for name, content in completed_tasks.iteritems():
-        print(name + ' = ' + str(len(content)))
+    # Compare completed tasks to find overlapping tasks
+    print("Finding overlapping tasks...")
+    overlapping_tasks = []
+    for p_task in completed_public_tasks:
+        if p_task not in overlapping_tasks:
+            p_task_location = get_location(p_task)
+            for i_task in completed_internal_tasks:
+                i_task_location = get_location(i_task)
+                if p_task_location == i_task_location:
+                    overlapping_tasks.append(p_task)
+    print("  overlapping_tasks = %s" % len(overlapping_tasks))
+
+    # Write to file
+    print("Writing overlapping tasks...")
+    with open(outfile, 'w') as f:
+        json.dump(overlapping_tasks, f)
+    print("Done.")
 
     # Successful
     return 0
