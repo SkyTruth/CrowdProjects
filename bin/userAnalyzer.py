f299e764201c15b61c0013bf4fadbb8e698c11ac
Kevin Wurster <kevin@skytruth.org>
2014-05-20 18:02:03 -0400

    Functional but needs to be vetted

--- bin/userAnalyzer.py
+++ bin/userAnalyzer.py
@@ -35,8 +35,16 @@
 # =================================================================================== #
 
 
+"""
+./userAnalyzer.py --sample=100 ../FrackFinder/Global_QAQC/dartfrog/transform/public/tasks/task_run.json
+"""
+
+
+from __future__ import division
+
 import sys
 import json
+from os import linesep
 from datetime import datetime
 from pprint import pprint
 from os.path import isfile
@@ -86,7 +94,7 @@ def print_usage():
     """
 
     print("")
-    print("Usage: %s --help-info [options]" % basename(__file__))
+    print("Usage: %s --help-info task_run.json outfile.csv" % basename(__file__))
     print("")
 
     return 1
@@ -172,12 +180,57 @@ def date2datetime(input_datetime_stamp):
     Convert PyBossa's XML formatted datetime to a Python datetime object
 
     Note that microseconds are dropped completely
+
+    This comes from the task_run.json file: task_run['created / finish_time']
     """
 
-    return datetime.strptime(input_datetime_stamp, "%Y-%m-%dT%H:%M:%S.%f").replace(microsecond=0)
+    return datetime.strptime(input_datetime_stamp, "%Y-%m-%dT%H:%M:%S.%f")
+
+
+def unix2datetime(input_unix_datetime_stamp):
+
+    """
+    Convert PyBossa's Unix formatted datetime to a Python datetime object
+
+    This comes from the task_run.json file: task_run['info']['timings['presentTask / reportAnswer']
+    """
+
+    return datetime.fromtimestamp(input_unix_datetime_stamp)
+
+
+def datetime_formatter(input_datetime_object, strfmt='%Y-%m-%d %H:%M:%S.%f'):
+
+    """
+    Convert a datetime object to a single normalized time format readable by Excel
+    """
+    if input_datetime_object is None:
+        return None
+    else:
+        return input_datetime_object.strftime(strfmt)
+
+
+def row_formatter(row, qualifier='"'):
+
+    """
+    Format a row in a consistent way
+    """
+
+    output_row = []
+    for item in row:
+        if item is None:
+            item = '""'
+        else:
+            item = qualifier + str(item) + qualifier
+        output_row.append(item)
+
+    return output_row
 
 
 def main(args):
+
+    """
+    Main command line logic
+    """
 
     #/* ======================================================================= */#
     #/*     Set Defaults
@@ -185,6 +238,11 @@ def main(args):
 
     # Input/output files
     input_task_runs_file = None
+    output_csv = None
+
+    # Parameters/configuration
+    sample_size = None
+    overwrite_outfile = False
 
     #/* ======================================================================= */#
     #/*     Parse Arguments
@@ -208,6 +266,18 @@ def main(args):
             elif arg in ('--license', '-license'):
                 return print_license()
 
+            # Additional parameters
+            elif '--overwrite' in arg:
+                i += 1
+                overwrite_outfile = True
+            elif '--sample=' in arg:
+                i += 1
+                try:
+                    sample_size = int(arg.split('=', 1)[1])
+                except ValueError:
+                    print("ERROR: Invalid sample - must be an int: %s" % arg)
+                    arg_error = True
+
             # Positional arguments and errors
             else:
 
@@ -215,6 +285,9 @@ def main(args):
                 if input_task_runs_file is None:
                     i += 1
                     input_task_runs_file = arg
+                elif output_csv is None:
+                    i += 1
+                    output_csv = arg
                 else:
                     i += 1
                     print("ERROR: Invalid argument: %s" % str(arg))
@@ -233,8 +306,17 @@ def main(args):
     if arg_error:
         print("ERROR: Did not successfully parse arguments")
         bail = True
+    if output_csv is None:
+        print("ERROR: Need an output file")
+        bail = True
+    if output_csv is not None and isfile(output_csv) and not overwrite_outfile:
+        print("ERROR: Outfile exists and overwrite=%s: %s" % (str(overwrite_outfile), output_csv))
+        bail = True
     if input_task_runs_file is None or not isfile(input_task_runs_file):
         print("ERROR: Can't find file: %s" % str(input_task_runs_file))
+        bail = True
+    if sample_size is not None and sample_size <= 0:
+        print("ERROR: Invalid sample size - must be > 0: %s" % str(sample_size))
         bail = True
     if bail:
         return 1
@@ -243,10 +325,20 @@ def main(args):
     #/*     Analyze Task Runs
     #/* ======================================================================= */#
 
+    # Update user
+    print("Analyzing task runs ...")
+
     # Load input file
     task_runs = None
     with open(input_task_runs_file) as f:
         task_runs = json.load(f)
+
+    # If we're doing a sample, grab it
+    if sample_size is not None:
+        if sample_size > len(task_runs):
+            sample_size = len(task_runs)
+            print("WARNING: Sample size larger than input - adjusted to: %s" % str(sample_size))
+        task_runs = task_runs[:sample_size]
 
     # Define containers
     stats = {}
@@ -268,50 +360,100 @@ def main(args):
             utype = 'ip'
             nuid = uip
 
-        # Collect user ID stats
-        stats[nuid] = {'uid_type': utype,
-                       'user_id': uid,
-                       'user_ip': uip,
-                       'n_uid': nuid}
+        # Collect IDs and add to the stats container
+        if nuid not in stats:
+            stats[nuid] = {'uid_type': utype,
+                           'user_id': uid,
+                           'user_ip': uip,
+                           'n_uid': nuid,
+                           'tr': [],
+                           'num_tasks_completed': 0,
+                           'start_date': None,
+                           'end_date': None,
+                           'avg_task_time_seconds': None}
 
-        # Get all task runs the user completed
-        completed_task_runs = get_task_runs(nuid, utype, task_runs)
-        stats[nuid]['tr'] = completed_task_runs
-        stats[nuid]['num_completed'] = len(completed_task_runs)
+        # Add to task runs and update count
+        stats[nuid]['tr'].append(tr)
+        stats[nuid]['num_tasks_completed'] += 1
 
-        # Compute metrics for completed task runs
+        # Compute time metrics for collected task runs
         start_date = None
         end_date = None
-        avg_completion_time = None
-        for c_tr in completed_task_runs:
+        avg_task_time = None
+        for c_tr in stats[nuid]['tr']:
 
-            c_sd = date2datetime(c_tr['created'])
-            c_ed = date2datetime(c_tr['finish_time'])
+            # Get the collected task_run's start/end date and start/end time
+            # Sometimes stuff isn't populated for whatever reason so we have to try/except each set
+            c_start_date = None
+            c_end_date = None
+            c_start_time = None
+            c_end_time = None
+            try:
+                c_start_date = date2datetime(c_tr['created'])
+                c_end_date = date2datetime(c_tr['finish'])
+            except KeyError:
+                pass
+            try:
+                c_start_time = unix2datetime(c_tr['info']['timings']['presentTask'])
+                c_end_time = unix2datetime(c_tr['info']['timings']['reportAnswer'])
+            except KeyError:
+                pass
 
-            # Compute average completion time per task run
-            if avg_completion_time is None:
-                avg_completion_time = c_ed - c_sd
+            # Handle start/end date
+            if start_date is None or c_start_date < start_date:
+                start_date = c_start_date
+            if end_date is None or c_end_date > end_date:
+                end_date = c_end_date
+
+            # Compute delta between start and end time for the task run
+            t_delta = None
+            if c_start_time is not None and c_end_time is not None:
+                t_delta = c_end_time - c_start_time
+
+            # Modify average time spent on tasks
+            if t_delta is not None and avg_task_time is None:
+                avg_task_time = t_delta.seconds
+            elif t_delta is not None:
+                avg_task_time = round((avg_task_time + t_delta.seconds) / 2, 2)
             else:
-                avg_completion_time = (avg_completion_time + c_ed - c_sd) / 2
+                avg_task_time = None
 
-            # Figure out if the date of the very first task the user completed and the very last task
-            if start_date is None or c_sd < start_date:
-                start_date = c_sd
-            if end_date is None or c_ed > end_date:
-                end_date = c_ed
+        # Dump start date, end date, and average task completion time into stats object
+        stats[nuid]['start_date'] = datetime_formatter(start_date)
+        stats[nuid]['end_date'] = datetime_formatter(end_date)
+        stats[nuid]['avg_task_time_seconds'] = avg_task_time  # This is already seconds so no need to format
 
-            # Dump collected information into stats
-            stats[nuid]['first_date'] = start_date.strftime("%m/%d/%Y %H:%M:%S")
-            stats[nuid]['end_date'] = end_date.strftime("%m/%d/%Y %H:%M:%S")
-            stats[nuid]['avg_time'] = str(avg_completion_time)
+    # Done analyzing task runs - update user
+    print("  - Done")
 
+    # Write output csv
+    print("Processing CSV rows ...")
 
+    # Define header structure and containers
+    header = ['n_uid', 'user_id', 'user_ip', 'uid_type', 'start_date', 'end_date', 'num_tasks_completed',
+              'avg_task_time_seconds', 'end_date', 'start_date']
+    output_rows = [header]
 
-            ####### FIX AVG TIME CALCULATOR #######
-            stats[nuid]['tr'] = None
-            pprint(stats[nuid])
-            return 1
+    # Convert the stats container to CSV rows
+    ri = 0
+    for uid, attributes in stats.items():
 
+        ri += 1
+        sys.stdout.write("\r\x1b[K" + "  %s/%s" % (str(ri), str(len(stats.keys()))))
+        sys.stdout.flush()
+
+        # Stick items into the proper place, according to the header, convert data types, and add text qualifiers
+        row = ['' for i in header]
+        for item in header:
+            row[header.index(item)] = attributes[item]
+        output_rows.append(row)
+    print("  - Done")
+
+    # Write the file
+    print("Writing output file: %s" % output_csv)
+    with open(output_csv, 'w') as f:
+        for row in output_rows:
+            f.write(','.join(row_formatter(row)) + linesep)
 
     # Success
     return 0
