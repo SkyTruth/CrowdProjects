bad067f34221b7963cd3198ed997918b4c4c200c
Kevin Wurster <kevin@skytruth.org>
2014-05-28 17:21:35 -0400

    Mostly functional - need to resolve how intersecting features are removed

--- FrackFinder/Final_Deliverable/bin/digitizer-task2shp.py
+++ FrackFinder/Final_Deliverable/bin/digitizer-task2shp.py
@@ -15,7 +15,11 @@ except ImportError:
     import osr
 
 
+# Get the script name
 __docname__ = basename(__file__)
+
+
+# Force OGR to use exceptions
 ogr.UseExceptions()
 
 
@@ -27,8 +31,10 @@ def print_usage():
     print("  --process-county -> Input task runs have a county attribute")
     print("  --process-year   -> Input task runs have a year attribute")
     print("  --overwrite -> Overwrite output.shp")
-    print("  --class=str -> Add a classification field with a uniform value")
+    print("  --class=str -> Add a classification field with a uniform value - use '%<field>' to pull from JSON")
     print("  --of=driver -> Specify output OGR driver - defaults to 'ESRI Shapefile'")
+    print("  --check-intersect   -> Don't check for intersecting geometries")
+    print("  --intersect-keep=str -> Keeps intersecting features based on their classified value")
     print("")
     return 1
 
@@ -79,9 +85,14 @@ def main(args):
     output_driver = 'ESRI Shapefile'
     feature_classification = None
 
+    # Additional processing
+    check_geom_intersect = False
+    check_geom_intersect_keep = None
+
     #/* ======================================================================= */#
     #/*     Containers
     #/* ======================================================================= */#
+
     infile = None
     outfile = None
 
@@ -96,17 +107,25 @@ def main(args):
         if arg in ('--usage', '--help', '--help-info'):
             return print_usage()
 
-        # Additional options
+        # OGR Configuration
+        elif '--of=' in arg:
+            output_driver = arg.split('=', 1)[1]
+
+        # Output file options
         elif arg == '--process-year':
             process_year = True
         elif arg == '--process-county':
             process_county = True
         elif arg == '--overwrite':
             overwrite_outfile = True
-        elif '--of=' in arg:
-            output_driver = arg.split('=', 1)[1]
         elif '--class=' in arg:
             feature_classification = arg.split('=', 1)[1]
+
+        # Additional processing
+        elif arg == '--check-intersect':
+            check_geom_intersect = True
+        elif '--keep-intersect=' in arg:
+            check_geom_intersect_keep = arg.split('=', 1)[1]
 
         # Positional arguments and errors
         else:
@@ -143,6 +162,9 @@ def main(args):
     if outfile is not None and isfile(outfile) and not overwrite_outfile:
         print("ERROR: Output file exists and overwrite=%s" % str(overwrite_outfile))
         bail = True
+    if check_geom_intersect_keep is not None and feature_classification is None:
+        print("ERROR: Need a classification in order to filter intersects")
+        bail = True
 
     if bail:
         return 1
@@ -154,23 +176,28 @@ def main(args):
     # Open JSON file
     with open(infile) as f:
         task_runs = json.load(f)
+    print("Found %s task runs" % str(len(task_runs)))
 
     # Delete output file if it exists
     if isfile(outfile):
+        print("Overwriting: %s" % outfile)
         driver = ogr.GetDriverByName(output_driver)
         driver.DeleteDataSource(outfile)
 
     # Create the datasource
+    print("Creating OGR datasource")
     driver = ogr.GetDriverByName(output_driver)
     datasource = driver.CreateDataSource(outfile)
     srs = osr.SpatialReference()
     srs.ImportFromEPSG(4326)
 
     # Create layers and define fields
+    print("Creating layers and fields")
     layer_name = basename(outfile).split('.', 1)[1]
     layer = datasource.CreateLayer(layer_name, srs, ogr.wkbMultiPolygon)
     field_definitions = [('selection', 254, ogr.OFTString),
-                         ('task_id', 10, ogr.OFTInteger)]
+                         ('task_id', 10, ogr.OFTInteger),
+                         ('intersect', 1, ogr.OFTInteger)]
     if process_year:
         field_definitions.append(('year', 10, ogr.OFTInteger))
     if process_county:
@@ -186,6 +213,7 @@ def main(args):
         layer.CreateField(field_object)
 
     # Loop through task runs and assemble output shapefile
+    print("Processing task runs...")
     for tr in task_runs:
         try:
             selection = str(tr['info']['selection'])
@@ -214,8 +242,17 @@ def main(args):
                     feature.SetField('year', tr['year'])
                 if process_county:
                     feature.SetField('county', str(tr['county']))
-                if feature_classification is not None:
+
+                # Normal feature classification is just a simple write but the % indicates that the classification
+                # is to be pulled from a field within the json
+                if feature_classification is not None and feature_classification[0] != '%':
                     feature.SetField('class', str(feature_classification))
+                elif feature_classification is not None and feature_classification[0] == '%':
+                    try:
+                        value = str(tr[feature_classification[1:]])
+                    except KeyError:
+                        value = None
+                    feature.SetField('class', value)
 
                 # Create the feature in the layer
                 layer.CreateFeature(feature)
@@ -223,12 +260,66 @@ def main(args):
             # Cleanup
             feature = None
 
+    # Loop through the output file and check for intersecting geometries
+    if check_geom_intersect:
+
+        print("Searching for intersecting geometries...")
+
+        intersect_count = 0
+
+        # Convert layer's features to a dictionary where keys are FID's and values are feature objects
+        feature_fids = {i.GetFID(): i for i in layer}
+
+        # Loop through layer
+        layer.ResetReading()
+        intersect_count = 0
+        for p_feature in layer:
+
+            # Make sure we're supposed to process this feature
+            p_fid = p_feature.GetFID()
+
+            # If we don't remove from the FID set, then an intersect will always be found
+            del feature_fids[p_fid]
+
+            # Loop through all the features we're comparing against and check for intersects
+            for i_feature, i_feature in feature_fids.copy().items():
+
+                # Prefixes: p=primary i=intersect - primary comes from outer loop
+                p_geom = p_feature.GetGeometryRef()
+                i_geom = i_feature.GetGeometryRef()
+                if p_geom.IsSimple() and i_geom.IsSimple():
+                    if p_geom.Intersect(i_geom):
+
+                        intersect_count += 1
+
+                        # Modify the primary feature in place
+                        p_feature.SetField('intersect', 1)
+                        layer.SetFeature(p_feature)
+
+                        # Modify the intersecting feature so it has the appropriate attributes
+                        i_feature.SetField('intersect', 1)
+                        layer.SetFeature(i_feature)
+
+                        # Filter out / delete one of the intersecting features if explicitly told to do so
+                        if check_geom_intersect_keep is not None:
+                            if p_feature.GetField('class') is i_feature.GetField('class'):
+                                print("WARNING: Could not filter intersect: homogeneous class")
+                            elif p_feature.GetField('class') != check_geom_intersect_keep:
+                                layer.DeleteFeature(p_feature.GetFID())
+                            elif i_feature.GetField('class') != check_geom_intersect_keep:
+                                layer.DeleteFeature(i_feature.GetFID())
+
+        # Update user
+        print("Found %s intersecting geometries" % str(intersect_count))
+
     # Cleanup
     driver = None
     datasource = None
     srs = None
 
     # Success
+
+    print("Done.")
     return 0
 
 
